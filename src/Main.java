76db630fca1b76967fece6dedaefb222a17d0f86
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-15 01:15:17 +0530

    created main.java entry point of the system

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static final String url = "jdbc:mysql://localhost:3306/hospital_sys";
+    public static final String user = "root";
+    public static final String pass = "root";
+
+
+    public static void main(String[] args) {
+        
+    }
+}
