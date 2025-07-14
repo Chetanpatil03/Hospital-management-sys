96db3692cd5965e7ef775b6d289ef778fa1b787d
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-15 01:28:21 +0530

    created addpatient method which is used to add patient in the system

--- src/Patient.java
+++ src/Patient.java
@@ -1,5 +1,8 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.SimpleTimeZone;
 
 public class Patient {
     private Connection connection;
@@ -10,5 +13,33 @@ public class Patient {
         this.connection = connection;
     }
 
-    
+    public void addPatient(){
+        System.out.print("Enter Patient name : ");
+        String name = sc.next();
+        System.out.print("Enter Patient age : ");
+        int age = sc.nextInt();
+        System.out.print("Enter Patient gender : ");
+        String gender = sc.next();
+
+        try {
+            String sql = "insert into patient(name,age,gender) values (?,?,?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,name);
+            preparedStatement.setInt(2,age);
+            preparedStatement.setString(3,gender);
+
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0){
+                System.out.println("Data inserted ");
+            }
+            else {
+                System.out.println("Failed to insert data");
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
