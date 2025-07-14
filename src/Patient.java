7b60a8b97487fe445516f3b5885448e83d3f73b4
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-15 01:46:55 +0530

    created viewPatient method to get information about current patient in db

--- src/Patient.java
+++ src/Patient.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.SimpleTimeZone;
@@ -31,14 +32,43 @@ public class Patient {
 
             int affectedRows = preparedStatement.executeUpdate();
             if (affectedRows > 0){
-                System.out.println("Data inserted ");
+                System.out.println("Patient added Successfully...");
             }
             else {
-                System.out.println("Failed to insert data");
+                System.out.println("Failed to add patient...");
             }
 
 
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void viewPatient(){
+        String sql = "select * from patient";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            System.out.println("Patients : \n");
+            System.out.println("+----------+------------------+-----------+------------+");
+            System.out.println("|    Id    |       Name       |    age    |   Gender   |");
+            System.out.println("+----------+------------------+-----------+------------+");
+
+            while (resultSet.next()){
+                int id= resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String gender = resultSet.getString("gender");
+
+                System.out.printf("|%-11d|%-19s|%-12d|%-13s|\n",id,name,age,gender);
+
+            }
+            System.out.println("+----------+------------------+-----------+------------+");
+
+        }
+        catch (SQLException e){
             e.printStackTrace();
         }
     }
