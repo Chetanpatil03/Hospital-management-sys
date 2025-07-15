82f0c0db5db4b3f9ca393f5075bab9f9f09e14ca
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-15 18:41:23 +0530

    created book appointment and doctor available to book an appointment

--- src/Main.java
+++ src/Main.java
@@ -68,7 +68,73 @@ public class Main {
 
     }
 
-    public static void bookAppointment(Connection conn){
-        String sql = "insert into "
+    public static void bookAppointment(Connection conn,Scanner sc,Patient patient,Doctor doctor){
+        System.out.print("Enter Patient id : ");
+        int patientId = sc.nextInt();
+
+        System.out.print("Enter Doctor id : ");
+        int doctorId = sc.nextInt();
+
+        System.out.println("Enter appointment date [yyyy-mm-dd] : ");
+        String appointmentDate = sc.next();
+        try{
+
+            if (patient.getPatientByID(patientId) && doctor.getDoctorsByID(doctorId)){
+                if (checkDoctorAvailability(doctorId,appointmentDate,conn)){
+                    String appointmentQuery = "insert into appointments(patient_id,doctor_id,appointment_date) values(?,?,?)";
+
+                    PreparedStatement preparedStatement = conn.prepareStatement(appointmentQuery);
+                    preparedStatement.setInt(1,patientId);
+                    preparedStatement.setInt(2,doctorId);
+                    preparedStatement.setString(3,appointmentDate);
+
+                    int affectedRows = preparedStatement.executeUpdate();
+                    if(affectedRows > 0){
+                        System.out.println("Appointment Booked");
+                    }
+                    else {
+                        System.out.println("Failed to book Appointment");
+                    }
+                }
+                else {
+                    System.out.println("Doctor not available on this date ");
+                }
+            }
+            else {
+                System.out.println("Either Doctor or Patient Doesn't Exist : ");
+            }
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean checkDoctorAvailability(int doctorId,String appointmentDate,Connection conn){
+        String query = "select count(*) from appointments where doctor_id = ? and appointment_date = ?";
+        try{
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1,doctorId);
+            preparedStatement.setString(2,appointmentDate);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+               int count = resultSet.getInt(1);
+               if (count == 0){
+                   return true;
+               }
+               else {
+                   return false;
+               }
+            }
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+
+        return false;
     }
 }
